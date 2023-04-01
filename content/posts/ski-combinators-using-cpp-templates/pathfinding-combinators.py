861256511047cbde384d4a717f783e8105281dfe
Kittenlover229 <artur.roos@mail.ru>
2023-04-01 19:52:57 +0300

    Add a python implementation for faster iteration

---
+++ content/posts/ski-combinators-using-cpp-templates/pathfinding-combinators.py
@@ -0,0 +1,43 @@
+expr = "S(K(SI))K   ab"
+
+expr = expr.replace(' ', '')
+
+
+def parse(input: str) -> tuple[list[str], str]:
+    ls = []
+    while input:
+        match input[0]:
+            case "(":
+                parsed, input = parse(input[1:])
+                ls.append(parsed)
+            case ")":
+                return (ls, input[1:])
+            case _:
+                ls.append(input[0])
+                input = input[1:]
+    return (ls, "")
+
+
+def parse_ski(input: str) -> list[str | list]:
+    return parse(input)[0]
+
+
+def simplify(input: list[str | list]) -> list[str | list] | str:
+    print(input)
+    match input:
+        case ["I", x, *rest]:
+            return simplify([x] + rest)
+        case ["K", x, _, *rest]:
+            return simplify([x] + rest)
+        case ["S", x, y, z, *rest]:
+            return simplify([x, z, simplify([y, z])] + rest)
+        case [[*contents], *rest]:
+            return simplify(contents + rest)
+        case [*prev, [*contents]]:
+            return simplify(prev + contents)
+        case _:
+            return input
+
+
+parsed = parse_ski(expr)
+print(simplify(parsed))
