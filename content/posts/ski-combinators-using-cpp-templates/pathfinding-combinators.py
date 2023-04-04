eb399acc65239f1957ed028dc05fcdaf08340937
Kittenlover229 <artur.roos@mail.ru>
2023-04-04 22:02:39 +0300

    Add a simple bfs

--- content/posts/ski-combinators-using-cpp-templates/pathfinding-combinators.py
+++ content/posts/ski-combinators-using-cpp-templates/pathfinding-combinators.py
@@ -1,6 +1,8 @@
-expr = "S(K(SI))K ab"
+from copy import deepcopy
 
-expr = expr.replace(' ', '')
+expr = ""
+
+expr = expr.replace(" ", "")
 
 
 def parse(input: str) -> tuple[list[str], str]:
@@ -23,7 +25,6 @@ def parse_ski(input: str) -> list[str | list]:
 
 
 def simplify(input: list[str | list]) -> list[str | list] | str:
-    print(input)
     match input:
         case ["I", x, *rest]:
             return simplify([x] + rest)
@@ -33,20 +34,56 @@ def simplify(input: list[str | list]) -> list[str | list] | str:
             return simplify([x, z, simplify([y, z])] + rest)
         case [[*contents], *rest]:
             return simplify(contents + rest)
-        case [*prev, [*contents]]:
-            return simplify(prev + contents)
         case _:
             return input
 
-def stringify(input: list) -> str:
+
+def bfs(input: list[list | str], start: list[str], target: list[list | str]):
+    simple_target = simplify(target)
+    def mutate(combinator: str):
+        transformed = []
+        for i in range(len(input) + 1):
+            cp = deepcopy(input)
+            if i < len(input) and cp[i] == combinator:
+                continue
+            cp.insert(i, combinator)
+            transformed.append(cp)
+            try:
+                if simplify(cp + start) == simple_target:
+                    print('!!!!', cp)
+                    exit(-1)
+            except RecursionError:
+                continue
+        for i, item in enumerate(input):
+            if type(item) is list:
+                for trans in bfs(item, target=target):
+                    transformed.append(
+                        input[:i] + [trans] + input[i + 1 :]
+                    )
+        return transformed
+
+    all = mutate("S") + mutate("K") 
+    return all
+
+
+def stringify(input: list, toplevel=False) -> str:
     match input:
         case [[*ls], *rest]:
             return f"({stringify(ls)})" + stringify(rest)
         case [*ls]:
-            return ''.join(ls)
+            string = "".join(stringify(x) for x in ls)
+            return f"({string})" if not toplevel else string
         case _:
             return input
 
+
 parsed = parse_ski(expr)
-print(simple := simplify(parsed))
-print(stringify(simple))
+
+inputs = [parsed]
+for i in range(9):
+    for input in inputs[::]:
+        new_inputs = []
+        for x in bfs(input, ['a', 'b'], ['b', 'a']):
+            print(u := x)
+            new_inputs.append(u)
+        inputs = new_inputs
